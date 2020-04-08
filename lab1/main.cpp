c8d72bbbd0c06851c9896c0c0ba83c574c91ef3f
PiotrJanus95 <63019178+PiotrJanus95@users.noreply.github.com>
2020-04-08 03:42:50 +0200

    Update main.cpp

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -7,6 +7,7 @@ int main()
 	int kolumny, wiersze;
 	kolumny = wiersze = 1;
 	tab = nowa_tablica(kolumny, wiersze);
+	// tworzy tablice o jednym wierszu i jednej kolumnie aby mogla byc przekazana do funkcji menu(...)
 	menu(tab, &kolumny, &wiersze);
 	return 0;
 }
