16349136469cf562bb118f10a392f75fb0629d15
PiotrJanus95 <63019178+PiotrJanus95@users.noreply.github.com>
2020-06-05 20:46:50 +0200

    obsluga nowego typu

--- lab6/komorka.h
+++ lab6/komorka.h
@@ -20,13 +20,13 @@ public:
 	virtual ~Komorka() {};
 
 	// Operator sumy zwraca cos + a
-	virtual int operator+ (int& a) { return cos + a; };
+	virtual double operator+ (double& a) { return cos + a; };
 
 	// Operator porownania sprawdza czy cos < a
-	virtual bool operator< (int& a) { return cos < a; };
+	virtual bool operator< (double& a) { return cos < a; };
 
 	// Operator porownania  sprawdza czy cos > a
-	virtual bool operator> (int& a) { return cos > a; };
+	virtual bool operator> (double& a) { return cos > a; };
 
 	// Operator przypisania dla int
 	virtual void operator= (const int& a) {};
@@ -34,11 +34,17 @@ public:
 	// Operator przypisania dla string
 	virtual void operator= (const string& s) {};
 
+	// Operator przypisania dla double
+	virtual void operator= (const double& d) {};
+
 	// Operator rzutowania zwraca cos jako lancuch
 	virtual operator string() const { return to_string(cos); };
 
 	// Operator rzutowania zwraca cos
 	virtual operator int() const { return cos; };
+
+	// Operator rzutowania zwraca cos
+	virtual operator double() const { return double(cos); };
 
 	// Metoda zwraca typ komorki (int - 0)
 	virtual int zwroc_typ() { return 0; };
@@ -59,6 +65,13 @@ public:
 	  * @param[in] nowy - nowa wartosc
 	  */
 	virtual void zmien_wartosc(string nowy) {};
+
+	/**
+	  * Metoda zmienia wartosc komorki typu double
+	  *
+	  * @param[in] nowy - nowa wartosc
+	  */
+	virtual void zmien_wartosc(double nowy) {};
 };
 
 #endif
